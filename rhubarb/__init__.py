b4ac0b04574b5398a3aa4157c5d208cbacc51f44
David Hall <dev@davidhall.tech>
2021-11-04 16:02:54 +0000

    :tada: Initial commit

---
+++ rhubarb/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Rhubarb is a library that simplifies realtime streaming for a number of backends into a single API"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
