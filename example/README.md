e3e889b0d37c2b8f5c3f05c86b4ff06039adf841
David Hall <dev@davidhall.tech>
2021-11-05 22:15:59 +0000

    :books: add example

---
+++ example/README.md
@@ -0,0 +1,26 @@
+# Setup
+
+Install requirements in your venv
+
+```bash
+pip install -r requirements.txt
+```
+
+To install rhubarb locally:
+
+```bash
+python -m pip install .
+```
+
+To run the example app:
+
+
+```bash
+uvicorn app:app
+```
+
+Open in your browser:
+
+```
+http://127.0.0.1:8000
+```
---
+++ example/app.py
@@ -0,0 +1,82 @@
+import os
+
+from starlette.applications import Starlette
+from starlette.concurrency import run_until_first_complete
+from starlette.responses import HTMLResponse
+from starlette.routing import Route, WebSocketRoute
+
+from rhubarb import Rhubarb
+
+URL = os.environ.get("URL", "redis://localhost:6379/0")
+
+events = Rhubarb(URL)
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+async def homepage(_):
+    return HTMLResponse(html)
+
+
+async def room_consumer(websocket):
+    async for message in websocket.iter_text():
+        await events.publish(channel="chatroom", message=message)
+
+
+async def room_producer(websocket):
+    async with events.subscribe(channel="chatroom") as subscriber:
+        async for event in subscriber:
+            await websocket.send_text(event.message)
+
+
+async def ws(websocket):
+    await websocket.accept()
+    await run_until_first_complete(
+        (room_consumer, {"websocket": websocket}),
+        (room_producer, {"websocket": websocket}),
+    )
+
+
+routes = [
+    Route("/", homepage),
+    WebSocketRoute("/ws", ws, name="chatroom_ws"),
+]
+
+
+app = Starlette(
+    routes=routes,
+    on_startup=[events.connect],
+    on_shutdown=[events.disconnect],
+)
